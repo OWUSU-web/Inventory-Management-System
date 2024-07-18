0ec1c2f51959accfd861d69dfe1f5c6ab855f092
OWUSU-web <99048088+OWUSU-web@users.noreply.github.com>
2024-07-18 01:43:11 +0000

    first commit

---
+++ updateprice.php
@@ -0,0 +1,16 @@
+<?php
+$mysqli_hostname = "localhost:3308";
+$mysqli_user = "root";
+$mysqli_password = "";
+$mysqli_database = "liveedit";
+$conn = mysqli_connect($mysqli_hostname, $mysqli_user, $mysqli_password,$mysqli_database) 
+or die("Opps some thing went wrong");
+// mysqli_select_db($mysqli_database, $bd) or die("Opps some thing went wrong");
+
+include("db.php");
+$proid=$_POST['ITEM'];
+$itemprice=$_POST['itemprice'];
+mysqli_query($conn,"UPDATE inventory SET price='$itemprice'
+WHERE id='$proid'");
+header("location: tableedit.php#page=editprice");
+?>
